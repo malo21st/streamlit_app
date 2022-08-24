ba0f54939b7c4b4692dce571269eab3d184ad8d1
malo21st <44739330+malo21st@users.noreply.github.com>
2022-08-24 18:28:29 +0900

    Update sky_app.py

--- sky_app.py
+++ sky_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import pydeck as pdk
 import os
 
+st.set_page_config(layout="wide")
+
 # MAPBOX_API_KEY
 os.environ['MAPBOX_API_KEY'] = st.secrets["MAPBOX_API_KEY"]
 
@@ -40,31 +42,62 @@ st.title('日本の空港間流通量')
 GREEN_RGB = [0, 255, 0, 40]
 RED_RGB = [240, 100, 0, 40]
 TOOLTIP_TEXT = {"html": "<b>{dep} => {arr}</b><br>旅客数：{pasN}人<br>貨物量：{crgN}トン<br>距　離：{dist}km<br>頻　度：{freq}本／週"}
-st.pydeck_chart(pdk.Deck(
-    initial_view_state = pdk.ViewState(
-        latitude=34.0,
-        longitude=131.0,
-        bearing=0,
-        pitch=50,
-        zoom=4,
-    ),
 
-    layers = [
-        pdk.Layer(
-            "ArcLayer",
-            data=df_data,
-            get_width="3",
-            get_source_position=["dep_lon", "dep_lat"],
-            get_target_position=["arr_lon", "arr_lat"],
-            get_tilt=15,
-            get_source_color=RED_RGB,
-            get_target_color=GREEN_RGB,
-            pickable=True,
-            auto_highlight=True,
-        ),      
-    ],
-    tooltip = TOOLTIP_TEXT,
-))
+col1, col2 = st.columns()
+
+with col1:
+    st.pydeck_chart(pdk.Deck(
+        initial_view_state = pdk.ViewState(
+            latitude=34.0,
+            longitude=131.0,
+            bearing=0,
+            pitch=50,
+            zoom=4,
+        ),
+
+        layers = [
+            pdk.Layer(
+                "ArcLayer",
+                data=df_data,
+                get_width="3",
+                get_source_position=["dep_lon", "dep_lat"],
+                get_target_position=["arr_lon", "arr_lat"],
+                get_tilt=15,
+                get_source_color=RED_RGB,
+                get_target_color=GREEN_RGB,
+                pickable=True,
+                auto_highlight=True,
+            ),      
+        ],
+        tooltip = TOOLTIP_TEXT,
+    ))
+
+with col2:
+    st.pydeck_chart(pdk.Deck(
+        initial_view_state = pdk.ViewState(
+            latitude=34.0,
+            longitude=131.0,
+            bearing=0,
+            pitch=50,
+            zoom=4,
+        ),
+
+        layers = [
+            pdk.Layer(
+                "ArcLayer",
+                data=df_data,
+                get_width="3",
+                get_source_position=["dep_lon", "dep_lat"],
+                get_target_position=["arr_lon", "arr_lat"],
+                get_tilt=15,
+                get_source_color=RED_RGB,
+                get_target_color=GREEN_RGB,
+                pickable=True,
+                auto_highlight=True,
+            ),      
+        ],
+        tooltip = TOOLTIP_TEXT,
+    ))
 
 ## Table
 df_data
